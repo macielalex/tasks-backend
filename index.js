f258c5dd73dc25a60d873174b51db7cdf3ae6412
Alex Maciel <alexmaciel.mail@gmail.com>
2021-02-25 16:29:04 -0300

    criação dos middlewares users e taks

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const app = express()
+const db = require('./config/db')
+const consign = require('consign')
 
 
+consign()
+    .then('./config/middlewares.js')
+    .into(app)
 
+
+app.db = db
 
 app.listen(3000, ()=>{
     console.log('Executando')
